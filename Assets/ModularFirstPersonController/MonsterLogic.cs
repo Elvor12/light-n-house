fc61d4e5784da7145566f239cf08162c767805ce
Egor Reshetnikov <elvorresh12@gmail.com>
2025-07-26 02:13:12 +0300

    Advanced Monster

--- Assets/ModularFirstPersonController/MonsterLogic.cs
+++ Assets/ModularFirstPersonController/MonsterLogic.cs
@@ -8,10 +8,15 @@ public class MonsterLogic : MonoBehaviour
     public GameObject target;
     public float wanderDist = 10f;
     public float setWanderTimer = 100f;
+    public LineRenderer linerender;
+    public float lineLenght;
+    public Vector3 direction = Vector3.zero;
+    public Vector3 destination = Vector3.zero;
 
     public float maxDist = 5;
     public float minDist = 2;
     public float timer;
+    public float viewTimer = 2f;
     private Transform targetPos;
     private NavMeshAgent agent;
 
@@ -32,29 +37,32 @@ public class MonsterLogic : MonoBehaviour
 
     void Update()
     {
+        Vector3 velocity = agent.velocity;
+
+        if(destination != Vector3.zero) direction = Vector3.Lerp(direction, destination - transform.position, viewTimer * Time.deltaTime).normalized;
+
+        SetLine();
+
         timer -= Time.deltaTime;
 
-        if (timer <= 0 && !agent.hasPath)
+        NavMeshPath path = new NavMeshPath();
+
+        if(agent.CalculatePath(targetPos.position, path) && path.status == NavMeshPathStatus.PathComplete) 
         {
-            NavMeshPath path = new NavMeshPath();
-
-            if (agent.CalculatePath(targetPos.position, path) && path.status == NavMeshPathStatus.PathComplete)
-            {
-                agent.SetDestination(targetPos.position);
-                return;
-            }
-
+            destination = targetPos.position;
+        }
+        else if ((timer <= 0 && !agent.hasPath))
+        {   
             Vector3 target = ValidNavMeshPoint(transform.position, wanderDist, -1, maxDist, minDist, agent);
-
-            if (target != transform.position)
-            {
-                agent.SetDestination(target);
-            }
-            
+            destination = target;   
             timer = setWanderTimer;
         }
+        
+        if(Vector3.Distance(direction, (destination - transform.position).normalized) < 0.1f) 
+        {
+            agent.SetDestination(destination);
+        }
     }
-
     private static Vector3 ValidNavMeshPoint(Vector3 originPos, float dist, int mask, float maxPathLenght, float minDist, NavMeshAgent agent)
     {
         int tries = 10;
@@ -105,5 +113,13 @@ public class MonsterLogic : MonoBehaviour
         }
 
         return lenght;
+    }
+    private void SetLine() 
+    {
+        Vector3 startPos = transform.position;
+        Vector3 secondPos = startPos + direction.normalized * lineLenght;
+        linerender.positionCount = 2;
+        linerender.SetPosition(0, startPos);
+        linerender.SetPosition(1, secondPos);
     }
 }
