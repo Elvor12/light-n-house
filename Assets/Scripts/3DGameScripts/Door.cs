b303781eb764d3d816a027d0565d95e054a87ebe
Andrey Chetporov <achetporov@gmail.com>
2025-08-01 17:12:05 +0300

    add obj to demo scene

--- Assets/Scripts/3DGameScripts/Door.cs
+++ Assets/Scripts/3DGameScripts/Door.cs
@@ -1,11 +1,52 @@
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
     public bool isLocked = true;
+    public float openAngle = 90f;
+    public float duration = 1f;
+
+    private bool isOpen = false;
+    private bool isMoving = false;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    void Start()
+    {
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(0, openAngle, 0) * closedRotation;
+    }
 
     public void Unlock()
     {
         Debug.Log("Дверь разблокирована!");
+        ToggleDoor();
+    }
+    public void ToggleDoor()
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(RotateDoor(isOpen ? openRotation : closedRotation, isOpen ? closedRotation : openRotation));
+            isOpen = !isOpen;
+        }
+    }
+
+    IEnumerator RotateDoor(Quaternion fromRotation, Quaternion toRotation)
+    {
+        isMoving = true;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Slerp(fromRotation, toRotation, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = toRotation;
+        isMoving = false;
     }
 }
